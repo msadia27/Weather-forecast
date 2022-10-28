b80e034620f833fbf881b8f133eb9e3763642903
Madelane Sadia <madelanecsadia@gmail.com>
2022-10-27 21:50:11 -0400

    formhandler function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
+var cityInputEl = document.querySelector("#city");
+var citiesContainerEl = document.querySelector("#cities-container");
+
+var formSubmitHandler = function(event) {
+    // prevent page from refreshing
+    event.preventDefault();
+
+    // get value from input element
+    var cityName = cityInputEl.value.trim();
+
+    if(cityName) {
+      getCityName(cityName);
+
+      // clear old content
+      citiesContainerEl.textContent = "";
+      cityInputEl.value = "";
+    } else {
+      alert("Please search city name");
+    }
+  };
+
+
 var getUserCities = function(city) {
     // format the github api url
     var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}" + city + "/cities";
